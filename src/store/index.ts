3e318cdae437aa97e59c66a60e6899ce3fff7aa6
Gabriel Viana <viannadoze@gmail.com>
2022-08-02 11:28:02 -0300

    create folder src

---
+++ src/store/index.ts
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './modules/rootSaga';
+import rootReducer from './modules/rootReducer';
+import { ICartState } from './modules/cart/types';
+
+export interface IState {
+  cart: ICartState;
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
